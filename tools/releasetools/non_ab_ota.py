6843602369b311d40c2a99ae9bd96834829ce9c1
bencetari <bence.tari@gmail.com>
2021-10-19 21:14:39 +0200

    Basic Crystalization
    Change-Id: I5a2125dcdb56fda31664e0b6e8fc0ee434fd3589

--- tools/releasetools/non_ab_ota.py
+++ tools/releasetools/non_ab_ota.py
@@ -236,7 +236,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.Print("            ````````````````````` `` .   ");
   script.Print("               ````````````````          ");
   script.Print("");
-  script.Print("                    ArrowOS");
+  script.Print("                    CrystalOS");
   script.Print("");
   script.Print("=================================================");
   script.Print(" Android version  : %s"%(android_version));
